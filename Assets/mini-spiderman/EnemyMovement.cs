520d4992e3228b00d0d2d3456dcbafd3b587bd57
Doug Price <solipet@gmail.com>
2016-07-19 14:48:36 -0400

    Lecture 12: NavAgent for Spiderman movement

---
+++ Assets/mini-spiderman/EnemyMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+	GameObject player;
+	NavMeshAgent nav;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag ("Player");
+		nav = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nav.SetDestination (player.transform.position);
+	}
+}
